32a203faf534f500432c44b68460fe156172d926
Maxirix <166589012+Maxirix1@users.noreply.github.com>
2024-09-10 22:41:01 +0700

    add

---
+++ client/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./style/style.css?v=1.0"">
+</head>
+
+<body>
+    <header>
+        <div class=" dataMain">
+    <h2>HN xxxxxx</h2>
+    <h2>คุณ xxx xxx</h2>
+    </div>
+    <div class="language">
+        
+        <select class="dropdownLang">
+            <option>Thailand</option>
+            <option>English</option>
+        </select>
+        <div class="textLang">
+            <p>Thailand</p>
+        </div>
+    </div>
+
+    </header>
+
+    <div class="container">
+        <div class="contenthead">
+            <div class="logo">
+                <img src="./assets/logoSmall.png" alt="">
+            </div>
+            <h1 class="textHead">จองคิวนัดหมาย</h1>
+        </div>
+        <div class="textSelect">
+            <p>ระบุแผนก</p>
+        </div>
+        <div class="dropdown">
+
+
+            <select class="department" id="department">
+                <option value="volvo">อายุรกรรม</option>
+                <option value="saab">Saab</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="audi">Audi</option>
+            </select>
+        </div>
+
+    </div>
+    </body>
+
+</html>
