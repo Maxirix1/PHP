3872fdea4df3b5f05b308619b707f37279815412
AkashiZai <motherfcuker11@gmail.com>
2024-09-24 17:24:14 +0700

    final
    last time

--- client/home.php
+++ client/home.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             $output = '<div class="contentButton" style="display: flex; flex-wrap: wrap; gap: 10px; justify-content: center; margin-top: 20px;">';
             foreach ($mergedTimeSlots as $slot) {
-                $buttonColor = in_array($slot, $reservedTimes) ? '#0e6dc7' : 'gray';
+                $buttonColor = in_array($slot, $reservedTimes) ? '#0e6dc7' : '#ababab';
                 $output .= '<button class="buttonDate" style="background-color: ' . $buttonColor . ';">' . htmlspecialchars($slot) . '</button>';
             }
             $output .= '</div>';
@@ -72,33 +72,48 @@ try {
     <title>Home</title>
     <link rel="stylesheet" href="./style/style.css?v=1.0">
     <link rel="stylesheet" href="./style/responsive.css">
+    <link rel="icon" type="image/x-icon" href="./assets/logoHead.png">
 </head>
 
 <body>
-    <header>
+<header>
         <div class="dataMain">
-            <h2>HN <?= htmlspecialchars($_SESSION['hn']) ?></h2>
-            <a href="../logout.php">ออกจากระบบ</a>
+            <h2 style="font-size: 2rem; font-weight: 600;">HN <?= htmlspecialchars($_SESSION['hn']) ?></h2>
+            <!-- <h2>คุณ </h2> -->
+            <a href="../logout.php" style="
+            background-color: #fff;
+            font-weight: 600;
+            color: #0a5491;
+            padding: 1px 15px;
+            border-radius: 5px;
+            margin-top: 10px;
+            z-index: 999;
+            ">ออกจากระบบ</a>
         </div>
         <div class="language">
+
             <select class="dropdownLang">
                 <option value="th">Thailand</option>
                 <option value="en">English</option>
             </select>
         </div>
+
     </header>
 
     <div class="containerMain">
-        <div class="contenthead">
-            <h1>จองคิวนัดหมาย</h1>
+    <div class="contenthead">
+            <div class="logo">
+                <img src="./assets/logoSmall.png" alt="Logo">
+            </div>
+            <h1 class="textHead">จองคิวนัดหมาย</h1>
         </div>
 
         <div class="textSelect">
             <p>ระบุแผนก</p>
         </div>
         <div class="dropdown">
-            <select class="department" id="department">
-                <option>เลือกแผนก</option>
+            <select class="department" id="department" style="background-color: #fff;">
+                <option value="" disabled selected>เลือกแผนก</option>
                 <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
                     <option><?= htmlspecialchars($row['name']) ?></option>
                 <?php } ?>
@@ -114,7 +129,12 @@ try {
                         <path d="M0 0h48v48H0z" fill="none" />
                     </svg>
                 </button>
-                <h1 id="monthDisplay" class="container"></h1>
+
+                <div class="dataMonthHead">
+                    <img src="./assets/calendar.png" alt="Calendar Icon">
+                    <p id="monthDisplay"></p>
+                </div>
+                
                 <button id="nextDates"><svg height="48" viewBox="0 0 48 48" width="48"
                         xmlns="http://www.w3.org/2000/svg">
                         <path d="M12 36l17-12-17-12v24zm20-24v24h4V12h-4z" />
@@ -124,17 +144,16 @@ try {
             <div id="dateContainer" class="container"></div>
 
 
-            <div class="headText">
-                <h2 class="textSelectTime">เลือกเวลา</h2>
+            <div>
+                <p class="text">เลือกเวลา</p>
             </div>
 
             <div class="containerDateSelect">
-                <div class="selectTime" style="padding:8px 25px;">
-                    <div class="containerDateSelect"></div>
+                <div class="selectTime">
                 </div>
             </div>
 
-            <div class="submit" style="margin-top: 20px;">
+            <div class="submit" style="margin: 0 20px;">
                 <button class="rounded">ยืนยัน</button>
             </div>
         </div>
